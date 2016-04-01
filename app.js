9120e27a96b59dcd2d32d21fbaca31ec3f2ef053
Alexander Seliverstov <seliverstov.a@gmail.com>
2016-04-01 15:39:45 +0300

    fix: Fix incorrect image loading

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const PORT = process.env.PORT || 8787;
 const TARGET_URL = 'http://cdn.animenewsnetwork.com';
 
 function handleRequest(origin_request, origin_response){
-  cloudscraper.get(TARGET_URL+origin_request.url, function(error, response, body) {
+  cloudscraper.request({
+    method: 'GET',
+    url: TARGET_URL+origin_request.url,
+    encoding: null
+  }, function(error, response, body) {
     if (error) {
       console.log('Error occurred');
       origin_response.end('Error: ' + error);
     } else {
       console.log(response.statusCode,response.headers);
       origin_response.writeHead(response.statusCode,response.headers);
-      origin_response.end(body);
+      origin_response.end(body,'binary');
     }
   });
 
